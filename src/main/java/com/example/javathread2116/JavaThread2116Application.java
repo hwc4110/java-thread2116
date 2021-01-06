38d977f44e6b543221b80ff9f99d5a939e653955
huangweicheng <hwc4110@126.com>
2021-01-06 14:59:59 +0800

    [add]测试提交

---
+++ src/main/java/com/example/javathread2116/JavaThread2116Application.java
@@ -0,0 +1,13 @@
+package com.example.javathread2116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThread2116Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThread2116Application.class, args);
+	}
+
+}
